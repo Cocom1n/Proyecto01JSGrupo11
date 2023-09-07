a6031e7ea8e089144050b90941c6046498d2cbbf
Leandro Nieto <nleandro0juarg@gmail.com>
2023-09-07 00:13:55 -0300

    Funcionando calculadora propina
    Por ahora, solo se ven los resultados en la consola.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Leandro Nieto");
+document.getElementById("calc").onclick = function()
+{
+    let costo = Number(document.getElementById("costo").value);
+    let propinaPorcentaje = Number(document.getElementById("propina").value);
+
+    let propina = propinaPorcentaje/100*costo;
+    let total = costo + propina;
+
+    console.log("Propina: "+ propina);
+    console.log("Total: "+ total);
+}
